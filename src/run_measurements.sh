5b12cc0161b067b010da9bcaff7b5c6d097eb9f2
Mateus Rocha Mazzari <mateus.mazzari@usp.br>
2017-06-29 22:43:39 -0300

    fix run measurement

--- src/run_measurements.sh
+++ src/run_measurements.sh
@@ -21,7 +21,7 @@ mkdir results
 
 INSTANCES=$1
 mkdir results/mandelbrot_mpi_$INSTANCES
-perf stat -r $MEASUREMENTS mpirun -np 8 --hostfile hostfile 8 mandelbrot_mpi -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
+perf stat -r $MEASUREMENTS mpirun -np 8 --hostfile hostfile mandelbrot_mpi -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
 perf stat -r $MEASUREMENTS mpirun -np 8 --hostfile hostfile mandelbrot_mpi -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
 perf stat -r $MEASUREMENTS mpirun -np 8 --hostfile hostfile mandelbrot_mpi 0.175 0.375 -0.1 0.1 $SIZE >> elephant.log 2>&1
 perf stat -r $MEASUREMENTS mpirun -np 8 --hostfile hostfile mandelbrot_mpi -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
