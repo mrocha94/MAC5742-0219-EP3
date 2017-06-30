74f7a60d01cefd82167010d873ce6132a40ef936
Mateus Rocha Mazzari <mateus.mazzari@usp.br>
2017-06-29 22:41:14 -0300

    without io

--- src/mandelbrot_mpi_io.c
+++ src/mandelbrot_mpi_io.c
@@ -36,6 +36,16 @@ int colors[17][3] = {
     {16, 16, 16},
 };
 
+void allocate_image_buffer() {
+  int rgb_size = 3;
+  image_buffer =
+      (unsigned char **)malloc(sizeof(unsigned char *) * image_buffer_size);
+
+  for (int i = 0; i < image_buffer_size; i++) {
+    image_buffer[i] = (unsigned char *)malloc(sizeof(unsigned char) * rgb_size);
+  };
+};
+
 void init(int argc, char *argv[]) {
   if (argc < 6) {
     printf(
@@ -64,6 +74,41 @@ void init(int argc, char *argv[]) {
     pixel_width = (c_x_max - c_x_min) / i_x_max;
     pixel_height = (c_y_max - c_y_min) / i_y_max;
   };
+};
+
+void update_rgb_buffer(int iteration, int x, int y) {
+  int color;
+
+  if (iteration == iteration_max) {
+    image_buffer[(i_y_max * y) + x][0] = colors[gradient_size][0];
+    image_buffer[(i_y_max * y) + x][1] = colors[gradient_size][1];
+    image_buffer[(i_y_max * y) + x][2] = colors[gradient_size][2];
+  } else {
+    color = iteration % gradient_size;
+
+    image_buffer[(i_y_max * y) + x][0] = colors[color][0];
+    image_buffer[(i_y_max * y) + x][1] = colors[color][1];
+    image_buffer[(i_y_max * y) + x][2] = colors[color][2];
+  };
+};
+
+void write_to_file() {
+  FILE *file;
+  char *filename = "output_ompi.ppm";
+  char *comment = "# ";
+
+  int max_color_component_value = 255;
+
+  file = fopen(filename, "wb");
+
+  fprintf(file, "P6\n %s\n %d\n %d\n %d\n", comment, i_x_max, i_y_max,
+          max_color_component_value);
+
+  for (int i = 0; i < image_buffer_size; i++) {
+    fwrite(image_buffer[i], 1, 3, file);
+  };
+
+  fclose(file);
 };
 
 void compute_mandelbrot_kernel(int *local_iteration_buffer, int init_k, int chunksize) {
@@ -137,6 +182,7 @@ void compute_mandelbrot(int taskid, int numtasks) {
     for (j = 0; j < chunksize; j++) {
       x = (init_k + j) % i_x_max;
       y = (init_k + j) / i_x_max;
+      update_rgb_buffer(local_iteration_buffer[j], x, y);
     }
 
     for (i = 1; i < numtasks; i++) {
@@ -145,6 +191,7 @@ void compute_mandelbrot(int taskid, int numtasks) {
       for (j = 0; j < chunksize; j++) {
         x = (init_k + j) % i_x_max;
         y = (init_k + j) / i_x_max;
+        update_rgb_buffer(local_iteration_buffer[j], x, y);
       }
     }
   } else {
@@ -169,7 +216,15 @@ int main(int argc, char *argv[]) {
     exit(0);
   }
 
+  if (taskid == MASTER) {
+    allocate_image_buffer();
+  }
+
   compute_mandelbrot(taskid, numtasks);
+
+  if (taskid == MASTER) {
+    write_to_file();
+  }
 
   MPI_Finalize();
   return 0;
--- src/run_measurements.sh
+++ src/run_measurements.sh
@@ -7,8 +7,10 @@ SIZE=8192
 
 NAMES=('mandelbrot_mpi')
 
+
+
 make
-# mkdir results
+mkdir results
 
 # mkdir results/mandelbrot_seq
 # perf stat -r $MEASUREMENTS ./mandelbrot_seq -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
@@ -17,12 +19,12 @@ make
 # perf stat -r $MEASUREMENTS ./mandelbrot_seq -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
 # mv *.log results/mandelbrot_seq
 
-INSTANCES=1
+INSTANCES=$1
 mkdir results/mandelbrot_mpi_$INSTANCES
-perf stat -r $MEASUREMENTS mpirun -np 8 mandelbrot_mpi -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
-perf stat -r $MEASUREMENTS mpirun -np 8 mandelbrot_mpi -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
-perf stat -r $MEASUREMENTS mpirun -np 8 mandelbrot_mpi 0.175 0.375 -0.1 0.1 $SIZE >> elephant.log 2>&1
-perf stat -r $MEASUREMENTS mpirun -np 8 mandelbrot_mpi -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
+perf stat -r $MEASUREMENTS mpirun -np 8 --hostfile hostfile 8 mandelbrot_mpi -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
+perf stat -r $MEASUREMENTS mpirun -np 8 --hostfile hostfile mandelbrot_mpi -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
+perf stat -r $MEASUREMENTS mpirun -np 8 --hostfile hostfile mandelbrot_mpi 0.175 0.375 -0.1 0.1 $SIZE >> elephant.log 2>&1
+perf stat -r $MEASUREMENTS mpirun -np 8 --hostfile hostfile mandelbrot_mpi -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
 mv *.log results/mandelbrot_mpi_$INSTANCES
 
 rm *.ppm
